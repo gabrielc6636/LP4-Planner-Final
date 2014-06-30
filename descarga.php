cd16a04107f8a9148bf0da0059232dbbe76713bd
yale89 <yair.ale89@gmail.com>
2014-06-29 21:52:02 -0300

    mas cambios

--- descarga.php
+++ descarga.php
@@ -12,10 +12,27 @@ $resultado = 'Bienvenido, '.$_SESSION['fname'].' '.$_SESSION['lname'];
 <head> 
 <title>Academic Planner</title> 
 <link href="styles/sesion.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+a:link {
+	color: #D6D6D6;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #D6D6D6;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+<meta charset="utf-8">
 </head> 
 <body>
 						<img alt="fondo" src="img/bg.png"  id="full-screen-background-image" />
-    <div id="head">
+<div id="head">
     	<div class="logoplanner">
     		<a href="user_page.php"><img src="img/logoplanner_small.png" /></a>
         </div>
@@ -32,8 +49,9 @@ $resultado = 'Bienvenido, '.$_SESSION['fname'].' '.$_SESSION['lname'];
                        <li class='has-sub'><a href='#'><span>Consulta</span></a>
                           <ul>
                              <li><a href='user_page_consulta.php'><span>Materias Aprobadas</span></a></li>
-                             <li><a href='user_page_oferta.php'><span>Materias Ofertadas</span></a></li>
-                             <li class='last'><a href='user_page_habilitadas.php'><span>Materias Habilitadas</span></a></li>
+                             <li><a href='user_page_habilitadas.php'><span>Materias Pendientes</span></a></li>
+							 <li><a href='user_page_academica.php'><span>Oferta Academica</span></a></li>
+                             <li class='last'><a href='user_page_oferta.php'><span>Oferta Cursable</span></a></li>
                           </ul>
                        </li>
                        <li><a href='construccion.html'><span>Contacto</span></a></li>
@@ -50,16 +68,15 @@ $resultado = 'Bienvenido, '.$_SESSION['fname'].' '.$_SESSION['lname'];
     
     
 <div class="ss-form-container"> 
-	<div class="ss-form-heading" align="justify">
+	<div class="ss-form-heading" align="justify" style="color:#666">
     	
 
-    	<p> Descarga la APK de Android para llevar Academic Planner con vos en tu celular</p>
+   	  <p> Descarg&#225; la APK de Android para llevar Academic Planner con vos en tu celular.</p>
             
       
-        <p> Descipcion: <br>
-        La APK de Academic Planner consiste en una aplicacion util para celulares con sistema operativo	Android. La misma te permite hacer consultas de tu hisoria academica, las materias aprobadas y las materias a las que estas habilitado a cursar, ademas de ver el progreso de tu carrera y tiene la informacion actualizada de la pagina de Academic Plannertrae en donde quieras que estes con tu celular.<p>
-        
-        <p>Compatible con Android 2.3.6 en adelante</p>
+        <p> Descipci&#243;n: <br>
+      La APK de Academic Planner consiste en una aplicacion &#250;til para celulares con sistema operativo Android. La misma te permite hacer consultas de tu historia acad&#233;mica, las materias aprobadas y las materias a las que estas habilitado/a a cursar. Adem&#225;s pod&#233;s ver el progreso de tu carrera y consultar la misma informaci&#243;n que la p&#225;gina de Academic Planner en cualquier lugar donde est&#233;s con tu Smartphone.<p>
+   <p>Compatible con Android 2.3.6 en adelante</p>
         
         <div class="imagenes" align="center">
 		        <a href="img/caps/android01.png"><img src="img/caps/android01.png" height="200" width="150"/>
@@ -67,11 +84,11 @@ $resultado = 'Bienvenido, '.$_SESSION['fname'].' '.$_SESSION['lname'];
                 <a href="img/caps/android03.png"><img src="img/caps/android03.png" height="200" width="150"/>
         </div>
         
-    	<a href="planner.apk" ><h1 align="center">Descargar</h1></a> 
+    	<h1 align="center" ><a style="color:#F60" href="planner.apk" >Descargar</a></h1> 
                 
-        <p> Problemas con la instalacion del APK en Android? <a href="construccion.html">Clic aqui</a></p>
-        <p> Sugerencias/Comentarios: <a href="construccion.html">Clic aqui</a></p>		
-    </div>
+      <p>¿Problemas con la instalaci&#243;n del APK en Android? <a style="color:#F60" href="construccion.html">Clic aqu&#237;</a></p>
+        <p> Sugerencias/Comentarios: <a style="color:#F60" href="construccion.html">Clic aqu&#237;</a></p>		
+  </div>
 </div>
 
 	<div id="footer">
